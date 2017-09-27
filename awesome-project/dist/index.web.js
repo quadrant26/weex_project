9823e11b0fb51770b27bbef93091561ade31ff57
quadrant26 <quadrant26@163.com>
2017-09-27 14:47:01 +0800

    remove project

---
+++ awesome-project/dist/index.web.js
@@ -0,0 +1,3 @@
+// { "framework": "Vue"} 
+
+!function(e){function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}var t={};r.m=e,r.c=t,r.i=function(e){return e},r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:n})},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},r.p="",r(r.s=2)}({2:function(e,r){throw new Error("Module build failed: Error: ENOENT: no such file or directory, open 'D:\\git\\weex_project\\awesome-project\\temp\\index.js'")}});
